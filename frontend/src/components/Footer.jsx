340579961eec8b5e0be8837dd80441cd55ecbf9e
sumeetnachane <sumeetnachane@gmail.com>
2025-07-18 11:00:00 +0530

    firts-commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { assets } from "../assets/assetsfrontend";
 const Footer = () => {
   return (
     <div className="md:mx-10">
-      <div className="flex flec-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         {/* ------- Left side content ------- */}
         <div>
           <img className="mb-5 w-40 " src={assets.logo} alt="" />
